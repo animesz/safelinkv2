36b368474b692577030785c4473f20f7874b0696
animesz <animeszkun@gmail.com>
2017-01-21 22:42:51 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -23,7 +23,7 @@ if(x=="1"){xxx="/2017/01/template-1.html"}
 if(x=="2"){xxx="/2017/01/template-2.html"}
 if(x=="3"){xxx="/2017/01/template-3.html"}
 if(x=="4"){xxx="/2017/01/template-4.html"}
-if(x=="5"){xxx="/2017/01/template-5.html"}a_to_ve[i].href="http://linkdownloadchecker.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/01/template-5.html"}a_to_ve[i].href="http://linkdownloadchecker.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
