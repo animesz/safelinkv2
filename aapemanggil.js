a236fa4aa637a88d81ddf72eb579a13b85e731e1
animesz <animeszkun@gmail.com>
2017-06-29 20:47:31 +0700

    Update aapemanggil.js

--- aapemanggil.js
+++ aapemanggil.js
@@ -20,10 +20,10 @@ j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*5)+ 1);var xxx=null;
 if(x=="1"){xxx="/2017/06/anime.html"}
-if(x=="2"){xxx="/2017/06/google-drive.html}
-if(x=="3"){xxx="/2017/06/gmail.html}
+if(x=="2"){xxx="/2017/06/google-drive.html"}
+if(x=="3"){xxx="/2017/06/gmail.html"}
 if(x=="4"){xxx="/2017/06/boruto-the.html"}
-if(x=="5"){xxx="/2017/06/game-pc.html}a_to_ve[i].href="http://percobaansingkat.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/06/game-pc.html"}a_to_ve[i].href="http://percobaansingkat.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
